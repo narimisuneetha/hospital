e6185c5c1209b2571b5151d1001e29b46c1ec6a1
narimisuneetha <140491237+narimisuneetha@users.noreply.github.com>
2023-08-18 13:37:11 +0530

    Update mongoose.js

--- Hospital-management-nodejs-master/server/db/mongoose.js
+++ Hospital-management-nodejs-master/server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database connection URL with yours
-mailto:mongoose.connect("mongodb+srv://suneetha536marolix:giTze3rlvmVoPAm4@cluster0.mnv4bve.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://suneetha536marolix:giTze3rlvmVoPAm4@cluster0.mnv4bve.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = {mongoose};
